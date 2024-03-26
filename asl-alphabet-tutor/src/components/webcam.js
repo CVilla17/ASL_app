d3e25773f9eed2f1414ba55661939d20d7905559
Carlos Villa <villac@mit.edu>
2024-03-26 17:36:06 -0400

    adding bounding boxes to more accurately find hands for prediction

--- asl-alphabet-tutor/src/components/webcam.js
+++ asl-alphabet-tutor/src/components/webcam.js
@@ -1,11 +1,65 @@
 import React, { Component } from "react";
-import { useCallback, useRef, useState } from "react"; // import useCallback
+import { useCallback, useRef, useState, useEffect } from "react"; // import useCallback
 import Webcam from "react-webcam";
+import * as handTrack from "handtrackjs";
 
 const CustomWebcam = () => {
   const webcamRef = useRef(null); // create a webcam reference
   const [imgSrc, setImgSrc] = useState(null); // initialize it
   const [prediction, setPred] = useState(null);
+  const [model, setModel] = useState(null);
+  const [predictions, setPredictions] = useState([]);
+
+  useEffect(() => {
+    let isCancelled = false;
+
+    const loadModel = async () => {
+      const modelParams = {
+        flipHorizontal: false, // flip e.g for video
+        imageScaleFactor: 0.9, // reduce input image size for gains in speed.
+        maxNumBoxes: 2, // maximum number of boxes to detect
+        iouThreshold: 0.8, // ioU threshold for non-max suppression
+        scoreThreshold: 0.9, // confidence threshold for predictions.
+      };
+      const model = await handTrack.load(modelParams);
+      if (!isCancelled) {
+        setModel(model);
+      }
+    };
+
+    loadModel();
+
+    return () => {
+      isCancelled = true;
+    };
+  }, []);
+
+  const detect = useCallback(async () => {
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      const prediction = await model.detect(video);
+      setPredictions(prediction);
+    }
+  }, [model]);
+
+  useEffect(() => {
+    if (model !== null) {
+      const interval = setInterval(() => {
+        detect();
+      }, 100);
+      return () => clearInterval(interval);
+    }
+  }, [model, detect]);
 
   const capture = useCallback(() => {
     const imageSrc = webcamRef.current.getScreenshot();
@@ -58,12 +112,35 @@ const CustomWebcam = () => {
       {imgSrc ? (
         <img src={imgSrc} alt="webcam" />
       ) : (
-        <Webcam
-          height={600}
-          width={600}
-          ref={webcamRef}
-          screenshotFormat="image/jpeg"
-        />
+        <div style={{ position: "relative" }}>
+          <Webcam
+            height={600}
+            width={600}
+            ref={webcamRef}
+            screenshotFormat="image/jpeg"
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              zIndex: -1,
+            }}
+          />
+          {predictions.map((prediction, i) => (
+            <div
+              key={i}
+              style={{
+                position: "absolute",
+                top: prediction.bbox[1],
+                left: prediction.bbox[0],
+                height: prediction.bbox[3],
+                width: prediction.bbox[2],
+                borderWidth: 2,
+                borderColor: "red",
+                borderStyle: "solid",
+              }}
+            ></div>
+          ))}
+        </div>
       )}
       <div className="btn-container">
         {imgSrc ? (
